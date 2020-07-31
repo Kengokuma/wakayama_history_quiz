c9415497597f11f601187ced3a2c3c5ba72088be
Kengokuma <kengo171227@gmail.com>
2020-07-31 20:37:59 +0900

    master

---
+++ service_worker.js
@@ -0,0 +1,27 @@
+// キャッシュファイルの指定
+var CACHE_NAME = 'wakayama-history-quiz-caches';
+var urlsToCache = [
+    '/kengokuma.github.io/wakayama_history_quiz/',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
